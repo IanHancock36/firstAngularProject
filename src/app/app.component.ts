d744673d11d4572e612f27b5c37ab223ea0114e8
Ian Hancock <ianhancock@Ians-MacBook-Pro.local>
2024-02-06 16:57:57 -0600

    added home card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { HomeComponent } from './home/home.component'
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet,HomeComponent],
   template: `
   <main>
     <header class="brand-name">
--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
@@ -9,12 +10,14 @@ import { Component } from '@angular/core';
     HousingLocationComponent
   ],
   template: `
-    <p>
-      housing-location works!
-    </p>
+  <section class="listing">
+    <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
+    <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+    <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
+  </section>
   `,
   styleUrl: './housing-location.component.css'
 })
 export class HousingLocationComponent {
-
+@Input() housingLocation!: HousingLocation;
 }
