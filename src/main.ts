d6c553ca9d298eb1eae275e90f0f6ce7ad877f95
Ian Hancock <ianhancock@Ians-MacBook-Pro.local>
2024-02-07 13:54:57 -0600

    lesson 11

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { bootstrapApplication, provideProtractorTestingSupport } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
-import { routeConfig } from './app/app.routes';
+import routeConfig from './app/app.routes';
+
  
   bootstrapApplication(AppComponent,
     {
