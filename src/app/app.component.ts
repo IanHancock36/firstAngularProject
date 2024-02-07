1343ec64cd1c1876d73d3745ebc1fcd0364d29cd
Ian Hancock <ianhancock@Ians-MacBook-Pro.local>
2024-02-07 15:06:42 -0600

    less 12

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,18 @@ import { RouterModule } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, HomeComponent,RouterModule],
-  template: `
-    <main>
-      <a [routerLink]="['/']">
-        <header class="brand-name">
-          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-        </header>
-      </a>
-      <section class="content">
-        <router-outlet></router-outlet>
-      </section>
-    </main>
-  `,
+template: `
+  <main>
+    <a [routerLink]="['/']">
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+      </header>
+    </a>
+    <section class="content">
+      <router-outlet></router-outlet>
+    </section>
+  </main>
+`,
   styleUrl: './app.component.css',
 })
 export class AppComponent {
